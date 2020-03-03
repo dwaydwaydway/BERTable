ff9af740656239d3be8d04931abcfc62ad07406a
dwaydwaydway <tim8733123@gmail.com>
2020-03-03 12:07:32 +0800

    update

---
+++ exp/forest_cover_type/get_data.py
@@ -0,0 +1,57 @@
+"""Downloads and prepares the Forest Covertype dataset."""
+import argparse
+import gzip
+import os
+import shutil
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import wget
+import sys
+from box import Box
+from pathlib import Path
+
+import ipdb
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-c', '--config', dest='config_path',
+        default='./config.yaml', type=Path,
+        help='the path of config file')
+    args = parser.parse_args()
+    return vars(args)
+
+
+def main(config_path):
+    config = Box.from_yaml(config_path.open())
+    if not os.path.exists(config.data_dir / 'covtype.csv'):
+        os.makedirs(config.data_dir)
+
+        filename = wget.download(config.url)
+        with gzip.open(filename, 'rb') as f_in:
+            with open(config.data_dir / 'covtype.csv', 'wb') as f_out:
+                shutil.copyfileobj(f_in, f_out)
+    df = pd.read_csv(config.data_dir / 'covtype.csv')
+    n_total = len(df)
+
+    indices = {}
+    indices['train'], indices['test'] = train_test_split(
+        range(n_total), test_size=config.test_size, random_state=0)
+    indices['train'], indices['val'] = train_test_split(
+        indices['train'], test_size=config.val_size, random_state=0)
+
+    dataframe = {}
+    for split in {'train', 'test', 'val'}:
+        dataframe['split'] = df.iloc[indices['split']]
+        if split == 'train':
+            dataframe['split'] = dataframe['split'].sample(frac=1)
+        dataframe['split'].to_csv(
+            config.data_dir / f'{split}_covertype.csv', index=False, header=False)
+
+
+if __name__ == '__main__':
+    with ipdb.launch_ipdb_on_exception():
+        sys.breakpointhook = ipdb.set_trace
+        args = parse_args()
+        main(**args)
--- exp/forest_cover_type/gs_xgboost.py
+++ exp/forest_cover_type/gs_xgboost.py
@@ -1,41 +1,56 @@
 import xgboost as xgb
-
+import pandas as pd
+import time
 from sklearn.model_selection import GridSearchCV
+import sys
+import ipdb
 
-x_train, y_train, x_valid, y_valid, x_test, y_test =  # load datasets
 
-clf = xgb.XGBClassifier()
+def main():
+    x, y = {}, {}
+    for split in {'train', 'test', 'val'}:
+        x[split] = pd.read_csv(f'data/{split}_covertype.csv', header=None)
+        y[split] = x[split].iloc[len(x[split].columns)-1]
+        x[split] = x[split].drop(len(x[split].columns)-1, axis=1)
 
-param_grid = {
-    'max_depth': list(range(5, 50, 2)),
-    'learning_rate': list(range(0, 1, 0.001)),
-    'subsample': list(range(0, 1, 0.001)),
-    'colsample_bytree': list(range(0, 1, 0.001)),
-    'colsample_bylevel': list(range(0, 1, 0.001)),
-    'colsample_bynode': list(range(0, 1, 0.001)),
-    'min_child_weight': [0.5, 1.0, 3.0, 5.0, 7.0, 10.0],
-    'gamma': list(range(0.1, 5, 0.01)),
-    'reg_lambda': list(range(0.1, 10, 0.5)),
-    'n_estimators': list(range(50, 500, 5))}
+    clf = xgb.XGBClassifier()
 
-fit_params = {
-    'eval_metric': 'merror',
-    'objective': 'multi:softmax',
-    'early_stopping_rounds': 20,
-    'eval_set': [(x_valid, y_valid)]}
+    param_grid = {
+        'max_depth': list(range(5, 50, 2)),
+        'learning_rate': list(range(0, 1, 0.001)),
+        'subsample': list(range(0, 1, 0.001)),
+        'colsample_bytree': list(range(0, 1, 0.001)),
+        'colsample_bylevel': list(range(0, 1, 0.001)),
+        'colsample_bynode': list(range(0, 1, 0.001)),
+        'min_child_weight': [0.5, 1.0, 3.0, 5.0, 7.0, 10.0],
+        'gamma': list(range(0.1, 5, 0.01)),
+        'reg_lambda': list(range(0.1, 10, 0.5)),
+        'n_estimators': list(range(50, 500, 5))}
 
-rs_clf = GridSearchCV(clf, param_grid, n_iter=20,
-                            n_jobs=10, verbose=2, cv=2,
-                            fit_params=fit_params,
-                            scoring='neg_log_loss', refit=False, random_state=42)
-print("Grid search..")
-search_time_start = time.time()
-rs_clf.fit(x_train, y_train)
-print("Randomized search time:", time.time() - search_time_start)
+    fit_params = {
+        'eval_metric': 'merror',
+        'objective': 'multi:softmax',
+        'early_stopping_rounds': 20,
+        'eval_set': [(x['val'], y['val'])]}
 
-best_score = rs_clf.best_score_
-best_params = rs_clf.best_params_
-print("Best score: {}".format(best_score))
-print("Best params: ")
-for param_name in sorted(best_params.keys()):
-    print('%s: %r' % (param_name, best_params[param_name]))
+    rs_clf = GridSearchCV(clf, param_grid, n_iter=20,
+                          n_jobs=10, verbose=2, cv=2,
+                          fit_params=fit_params,
+                          scoring='neg_log_loss', refit=False, random_state=42)
+    print("Grid search..")
+    search_time_start = time.time()
+    rs_clf.fit(x['train'], y['train'])
+    print("Randomized search time:", time.time() - search_time_start)
+
+    best_score = rs_clf.best_score_
+    best_params = rs_clf.best_params_
+    print("Best score: {}".format(best_score))
+    print("Best params: ")
+    for param_name in sorted(best_params.keys()):
+        print('%s: %r' % (param_name, best_params[param_name]))
+
+
+if __name__ == '__main__':
+    with ipdb.launch_ipdb_on_exception():
+        sys.breakpointhook = ipdb.set_trace
+        main()
